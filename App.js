8dbf156093861277e1b53067ed5f717391b909f8
mrAdamLee <adamangel@southern.edu>
2024-02-01 21:18:52 -0500

    not list now working, but wildly formatted

--- App.js
+++ App.js
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import { View, TextInput, Button, StyleSheet } from 'react-native';
 import axios from 'axios';
 import NoteInput from './src/components/NoteInput';
+import NoteList from './src/components/NoteList';
 const App = () => {
-  return <NoteInput />; 
+  return <NoteList />; 
   
 };
 
--- src/services/noteService.js
+++ src/services/noteService.js
@@ -10,7 +10,7 @@ export const submitNote = async (noteText) => {
 
 export const listNotes = async () => {
   try {
-    const response = await axios.get('http://192.168.86.27:3000/notes');
+    const response = await axios.get('http://192.168.1.37:3000/notes');
     return response.data; // Assuming the server returns the list of notes
   } catch (error) {
     console.error(error);
